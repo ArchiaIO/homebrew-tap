e85a8277a5267da85a565fe46cafed072c3918de
Bart Carroll <103963480+bartCarroll@users.noreply.github.com>
2026-05-29 10:46:22 -0500

    Add archia 0.20.0-rc1 formula (#1)

--- Formula/archia.rb
+++ Formula/archia.rb
@@ -1,20 +1,26 @@
 class Archia < Formula
-  desc "Archia CLI"
-  homepage "https://archia.io"
-  version "0.19.1"
+  desc "Command-line interface for Archia"
+  homepage "https://archia.app"
+  version "0.20.0-rc1"
+  license :cannot_represent
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://archia-distribution.s3.us-east-1.amazonaws.com/archia-cli/archia-v0.19.1-darwin-arm64.tar.gz"
-    sha256 "8f2895d5ee5c3f257e66f0397baa48b02bbbc715d1272dea3761129dbbeb6ee1"
+    url "https://archia-distribution.s3.us-east-1.amazonaws.com/archia-cli/archia-0.20.0-rc1-aarch64-apple-darwin.tar.gz"
+    sha256 "48029a85b20fd42e4085843b752b2f6e7fa72cec780b820ef4661e8a8f4a7b0c"
   else
-    odie "Archia only supports Apple Silicon Macs."
+    odie "Archia Homebrew RC1 currently supports Apple Silicon Macs only."
   end
 
   def install
-    bin.install "archia"
+    bin.install "bin/archia"
+    man1.install "share/man/man1/archia.1"
+    bash_completion.install "share/bash-completion/completions/archia"
+    zsh_completion.install "share/zsh/site-functions/_archia"
+    fish_completion.install "share/fish/vendor_completions.d/archia.fish"
   end
 
   test do
-    system "#{bin}/archia", "--version"
+    assert_match "ARCHIA(1)", shell_output("#{bin}/archia man")
+    assert_match "_archia", shell_output("#{bin}/archia completion zsh")
   end
 end
