c1b0a40a4149c13155bb47c63d94c6933eb2062b
AndrewRademacher <andrewrademacher@icloud.com>
2026-06-26 13:36:32 -0500

    archia 0.24.1

--- Formula/archia.rb
+++ Formula/archia.rb
@@ -4,8 +4,8 @@ class Archia < Formula
   license :cannot_represent
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://archia-distribution.s3.us-east-1.amazonaws.com/archia-cli/archia-0.21.0-aarch64-apple-darwin.tar.gz"
-    sha256 "af0ae12f0e74a749312a4a833162c52ab3f70006a86318e5f4ba4e698ccf9b61"
+    url "https://archia-distribution.s3.us-east-1.amazonaws.com/archia-cli/archia-0.24.1-aarch64-apple-darwin.tar.gz"
+    sha256 "d9c5766a8b80e739acd4432cd62e8c2c90a19ff11d85900f784000a4b33fbad8"
   else
     odie "Archia Homebrew currently supports Apple Silicon Macs only."
   end
